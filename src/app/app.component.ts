e4f577f51b7853b76335f61f226a7a61585e5cbe
Nikolay_R <nikolay5011995@gmail.com>
2018-08-12 12:59:32 +0300

    button binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-main',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public title: string = 'start title';
+  public changeTitleText(newTitleText: string): void {
+    this.title = newTitleText;
+  }
 }
